1621774eb774b6f60c4bf1bfcfe7f178163a966a
unknown <linder.arnaud@orange.fr>
2023-05-19 15:54:41 +0200

    Finis sauf le js du  menus burger

---
+++ Site/menus.js
@@ -0,0 +1,12 @@
+let menusBurger=document.getElementById("menusBurger");
+let menusFerme=document.getElementById("menuFerme");
+
+function ouvrireMenu(){
+    // menusBurger.classList.remove('hidden');
+    // menusFerme.classList.add('hidden')
+
+    console.log("test");
+}
+
+
+menuFerme.onclic=ouvrireMenu;
